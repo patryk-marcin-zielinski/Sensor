6d16734cbc826c49f301744a8f3b5e96dca146f4
Patryk Zielinski <pzielinski@valuelogic.one>
2018-10-12 15:15:36 +0200

    Merging Humidity in map

--- src/SensorReadout.hs
+++ src/SensorReadout.hs
@@ -9,14 +9,22 @@ import Text.RawString.QQ
 import Data.Map (Map)
 import qualified Data.Map as M
 
-data Humidity = Humidity Int | Fail deriving (Show)
+data Humidity a = Humidity a | Fail deriving (Show)
 type SensorId = String
 
-data Readout = Readout SensorId Humidity deriving (Show)
+data Readout a = Readout SensorId (Humidity a) deriving (Show)
+
+instance Functor Humidity where
+    fmap fa2b (Humidity a) = Humidity $ fa2b a
+    fmap _ Fail = Fail
+
+instance Applicative Humidity where
+    pure = Humidity
+    (Humidity fa2b) <*> a = fmap fa2b a
+    Fail <*> _ = Fail
 
 
-
-parseHumidity :: Parser Humidity
+parseHumidity :: Parser (Humidity Int)
 parseHumidity = (Humidity . fromIntegral) <$> integer <|> fmap (\_ -> Fail) (char 'N' *> char 'a' *> char 'N')
 
 parseHeader :: Parser String
@@ -25,22 +33,25 @@ parseHeader = do
     (skipSome $ char ',') <|> pure ()
     return header
 
-parseReadout :: Parser (SensorId, Readout)
+parseReadout :: Parser (SensorId, Humidity Int)
 parseReadout = do
     sensorId <- some (noneOf ",")
     skipSome $ char ','
     humidity <- parseHumidity
     (skipMany (oneOf "\n")) <|> pure ()
-    return $ (sensorId, Readout sensorId humidity)
+    return $ (sensorId, humidity)
+
+mergeReadout :: (Num a) => Humidity a -> Humidity a -> Humidity a
+mergeReadout a b = liftA2 (+) a b
 
 
-parseAllReadouts :: Parser (Map SensorId Readout)
+parseAllReadouts :: Parser (Map SensorId (Humidity Int))
 parseAllReadouts = do
     skipMany (oneOf "\n")
     some parseHeader
     skipMany (oneOf "\n")
     readouts <- some (parseReadout)
-    return $ M.fromListWith (<>) readouts
+    return $ M.fromListWith (mergeReadout) readouts
 
 
 eitherOr :: String
@@ -48,7 +59,8 @@ eitherOr = [r|
 sensor-id,humidity
 s1,100
 s2,NaN
-s3,200|]
+s3,200
+s1,200|]
 
 
 
